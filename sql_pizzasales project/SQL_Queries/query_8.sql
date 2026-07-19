87837e6add3a142c2cdbd18d0c54ead8ce507d3a
Manya Mishra <manyamishra82@gmail.com>
2026-07-19 23:38:20 +0530

    Delete sql_pizzasales project directory

--- sql_pizzasales project/SQL_Queries/query_8.sql
+++
@@ -1,8 +0,0 @@
--- Join relevant tables to find the category-wise distribution of pizzas
--- (Shows the number of distinct pizza items within each category(e.g., 9 Veggie, 8 Classic)
-
-SELECT 
-    category, COUNT(name)
-FROM
-    pizza_types
-GROUP BY category;
